4eda656f73e31d54ef65ffc272ae111c1f6ceba0
JamesVanWaza <jgacuca@gmail.com>
2023-10-02 17:31:31 -0400

    Migrated styles to use the new scss file management

--- src/js/index.js
+++ src/js/index.js
@@ -16,21 +16,23 @@ function footer() {
 
     // assign it a class
     footerDiv.classList.add("footer");
+    footerDiv.classList.add("mt-auto");
+    footerDiv.classList.add("text-center");
+    footerDiv.classList.add("py-3");
+
 
     // gets the current date
     const copyright = new Date().getFullYear();
 
     // gets the copyright symbol
     const favicon = document.createElement("i");
-    favicon.classList.add("fa-solid", "fa-copyright");
+    favicon.classList.add("fas", "fa-copyright");
 
-    const text = document.createTextNode(" 2004 " + "- " + copyright);
-    const text2 = document.createTextNode(" Heart4Kids.org All Rights Reserved.");
+    const text = document.createTextNode(" 2020 " + "- " + copyright);
 
     // add the text node to the newly created div
     footerDiv.appendChild(favicon);
     footerDiv.appendChild(text);
-    footerDiv.appendChild(text2);
 
     // add the newly created element and its content into the DOM
     const newDiv = document.getElementById("div");
